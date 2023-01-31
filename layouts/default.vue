3f1d20a3c9dca4c012915cf6ad6bde97a55029da
rimbadi <rimbadi26@gmail.com>
2023-01-31 15:58:32 +0700

    second commit

---
+++ layouts/default.vue
@@ -0,0 +1,96 @@
+<template>
+  <v-app>
+    <v-navigation-drawer
+      v-model="sideDrawer"
+      fixed
+      app
+    >
+      <v-list>
+        <v-list-item
+          v-for="(item, i) in sideMenu"
+          :key="i"
+          :to="item.to"
+          router
+          exact
+        >
+          <v-list-item-action>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+    <v-app-bar 
+      :clipped-left="clipped" 
+      fixed 
+      app
+    >
+      
+      <v-toolbar-title>{{ title }}</v-toolbar-title>
+    </v-app-bar>
+
+    <v-main>
+      <v-container>
+        <Nuxt />
+      </v-container>
+    </v-main>
+    
+    <v-bottom-navigation
+      horizontal
+      height="10vh"
+      fixed
+      color="primary"
+      app>
+    <v-app-bar-nav-icon 
+      @click.stop="sideDrawer = !sideDrawer"
+      v-ripple="false" 
+      plain/>
+        <v-btn 
+          v-for="(item, i) in buttomMenu"
+            :key="i"
+            :to="item.to"
+            v-ripple="false"
+            plain>
+          <v-span>{{ item.title }}</v-span>
+          <v-icon>{{ item.icon }}</v-icon>
+        </v-btn>
+        <v-spacer />
+    </v-bottom-navigation>
+
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: 'DefaultLayout',
+  data() {
+    return {
+      sideDrawer: false,
+      sideMenu: [
+        {
+          icon: 'mdi-account',
+          title: 'Account',
+          to: '/account',
+        },
+        {
+          icon:'mdi-bell',
+          title:'Notification',
+          to:'/notification',
+        }
+      ],
+
+      buttomMenu: [
+        {
+          icon: 'mdi-application',
+          title: 'App',
+          to: '/',
+        }
+      ],
+      right: true,
+      title: 'Cashier',
+    }
+  },
+}
+</script>
